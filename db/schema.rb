18ff0eb13d682e4a529114ae9ea501a861260d07
Daniel Manuel <dmanueljr@gmail.com>
2015-07-05 01:48:20 -0700

    adds User, Upload, Tag and Sender models

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150705081039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "senders", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "tag_name"
+    t.string   "tag_value"
+    t.integer  "upload_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["upload_id"], name: "index_tags_on_upload_id", using: :btree
+
+  create_table "uploads", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "sender_id"
+  end
+
+  add_index "uploads", ["sender_id"], name: "index_uploads_on_sender_id", using: :btree
+  add_index "uploads", ["user_id"], name: "index_uploads_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_foreign_key "tags", "uploads"
+  add_foreign_key "uploads", "senders"
+  add_foreign_key "uploads", "users"
 end
