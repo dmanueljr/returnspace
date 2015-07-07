e193d5f18f0246b952b07066a00b386de0c1e41b
Andrew Ham <hampossible@gmail.com>
2015-07-07 11:57:07 -0700

    added google api client gem

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'google-api-client', :require => 'google/api_client'
+
 gem 'omniauth'
 
 gem "omniauth-google-oauth2"
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['330244966698-bppo4d0dm72p71fumgb30vd6ndfqu9s3.apps.googleusercontent.com'], ENV['LTjxPDYJ4vhraVmMX7BdDth7']
+  provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'],
+    {
+     access_type: 'offline',
+     scope: 'https://www.googleapis.com/auth/plus.login, https://www.googleapis.com/auth/userinfo.email',
+     redirect_uri: 'http://localhost:3000/auth/google_oauth2/callback'
+    }
 end
