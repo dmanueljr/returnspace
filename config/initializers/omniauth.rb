44edf2a920e468c0c85b57a88f4276b1c0bf2707
Andrew Ham <hampossible@gmail.com>
2015-07-07 16:00:15 -0700

    formatted omniauth indentation

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,12 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+
   provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'],
     {
      access_type: 'offline',
      scope: 'https://www.googleapis.com/auth/plus.login, https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/drive',
      redirect_uri: 'http://localhost:3000/auth/google_oauth2/callback'
     }
+
 end
