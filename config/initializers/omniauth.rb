2592326cfc3634139d172a846d6193b441d4e4c8
Andrew Ham <hampossible@gmail.com>
2015-07-06 18:07:22 -0700

    added and bundled figaro gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["330244966698-bppo4d0dm72p71fumgb30vd6ndfqu9s3.apps.googleusercontent.com"], ENV["LTjxPDYJ4vhraVmMX7BdDth7"]
+  provider :google_oauth2, ENV['330244966698-bppo4d0dm72p71fumgb30vd6ndfqu9s3.apps.googleusercontent.com'], ENV['LTjxPDYJ4vhraVmMX7BdDth7']
 end
