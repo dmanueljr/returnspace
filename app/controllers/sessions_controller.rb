43f0a8eba9dffe597dec964ff1bdccf2a491c0b2
Andrew Ham <hampossible@gmail.com>
2015-07-07 18:23:01 -0700

    added provider column to users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
-class SessionsController < ApplicationController
-  def create 
+class SessionsController < ApplicationController  
+  def create
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
-    has_many :uploads
-    has_many :senders, through: :uploads
+
+  has_many :uploads
+  has_many :senders, through: :uploads
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.id = auth.uid
+      user.first_name = auth.info.givenName
+      user.last_name = auth.info.familyName
+      user.save
+    end
+  end
+  
 end
