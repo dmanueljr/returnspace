5941348514857eae7c64d1e9901eeba11d102a84
Daniel Manuel <dmanueljr@gmail.com>
2015-07-07 22:40:01 -0700

    adds end to close sessions controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,13 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     @auth = env["omniauth.auth"]
-    # redirect_to root_path
+    redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
-    # redirect_to root_path
+    redirect_to root_path
+  end
 
   # def new
   # end
