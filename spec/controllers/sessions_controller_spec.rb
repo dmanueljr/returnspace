1ef1489b41df586b73b37f10b6e22212b74cce25
Daniel Manuel <dmanueljr@gmail.com>
2015-07-07 22:43:39 -0700

    fixes rspec merge conflict

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-<<<<<<< HEAD
-=======
   describe "GET #new" do
     it "returns http success" do
       get :new
@@ -25,5 +23,4 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
->>>>>>> 64a9bc026fd69c6a2efd8756f9c4b7dac9afaf79
 end
