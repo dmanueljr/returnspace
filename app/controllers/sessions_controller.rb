45e516ee51da5fa974ef7c8a9beadb529a398d60
Andrew Ham <hampossible@gmail.com>
2015-07-08 18:23:21 -0700

    added login and logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,18 @@
 class SessionsController < ApplicationController
+
+  def index
+  end
+
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    @auth = env["omniauth.auth"]
-    # redirect_to root_path
+    session[:auth] = env["omniauth.auth"]
+    redirect_to sessions_path
   end
 
   def destroy
     session[:user_id] = nil
-    # redirect_to root_path
+    session[:auth] = nil
+    redirect_to root_path
   end
 end
